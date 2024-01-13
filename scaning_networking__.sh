37f59de396a92b8ead5d22678587280c3c4e74ca
Valerii Chernadchuk <128724950+valer4ik312@users.noreply.github.com>
2024-01-13 19:28:32 +0200

    asd
    asd

---
+++ scaning_networking__.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Проверка наличия аргумента
+if [ -z "$1" ]; then
+    echo "Usage: $0 <target_ip>"
+    exit 1
+fi
+
+target_ip="$1"
+start_port=1
+end_port=9000
+
+for port in $(seq $start_port $end_port); do
+    (nc -z -w 1 $target_ip $port) >/dev/null 2>&1 && echo "Host: $target_ip, Port: $port is open"
+done
+
